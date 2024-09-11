8897f9b900eb6703713549614cab2eee98986292
Wahyudi-Satriawan-1B-TI <wahyudisatriawanh@gmail.com>
2024-09-11 09:35:50 +0700

    Penambahan fitur UTS dan UAS

--- Siakad26.java
+++ Siakad26.java
@@ -4,10 +4,9 @@ public class Siakad26 {
 
     public static void main(String[] args) {
         // deklarasi variable
-        String nama, nim;
-        char kelas;
+        String nama, nim, kelas;
         byte absen;
-        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+        double nilaiKuis, nilaiTugas, nilaiUTS, nilaiUAS, nilaiakhir;
 
         // membuat scanner baru
         Scanner sc = new Scanner(System.in);
@@ -18,7 +17,7 @@ public class Siakad26 {
         System.out.print("Masukkan NIM: ");
         nim = sc.nextLine();
         System.out.print("Masukkan kelas: ");
-        kelas = sc.nextLine().charAt(0);
+        kelas = sc.nextLine();
         System.out.print("Masukkan nomor absen: ");
         absen = sc.nextByte();
 
@@ -26,14 +25,16 @@ public class Siakad26 {
         nilaiKuis = sc.nextDouble();
         System.out.print("Masukkan nilai tugas: ");
         nilaiTugas = sc.nextDouble();
-        System.out.print("Masukkan nilai ujian: ");
-        nilaiUjian = sc.nextDouble();
+        System.out.print("Masukkan nilai UTS: ");
+        nilaiUTS = sc.nextDouble();
+        System.out.print("Masukkan nilai UAS: ");
+        nilaiUAS = sc.nextDouble();
 
-        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
+        nilaiakhir = (0.2 * nilaiKuis + 0.15 * nilaiTugas + 0.3 * nilaiUTS + 0.35 * nilaiUAS);
 
         System.out.println("Nama: " + nama + " NIM: " + nim);
         System.out.println("Kelas: " + kelas + " Absen: " + absen);
-        System.out.println("Nilai Akhir: " + nilaiAkhir);
+        System.out.println("Nilai Akhir: " + nilaiakhir);
 
     }
 }
